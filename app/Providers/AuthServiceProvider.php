4555cb669d3308d39576ad23b47e68620e08cd6d
RicardoMorato <rmr@cin.ufpe.br>
2023-03-02 20:47:13 -0300

    feat: add Gate for update and delete users

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -21,6 +23,12 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('update-user', function (User $user, int $id) {
+            return $user->id === $id;
+        });
+
+        Gate::define('delete-user', function (User $user, int $id) {
+            return $user->id === $id;
+        });
     }
 }
